82de5c6702c3dbbc73134784bbdd188d7f515efd
kaikySantos <info.kaiky@gmail.com>
2023-01-29 17:48:07 -0300

    feat: default theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import { Text } from 'react-native';
 import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
+
+import { NativeBaseProvider } from 'native-base';
+import { THEME } from 'src/theme';
+
+import { Text } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </NativeBaseProvider>
