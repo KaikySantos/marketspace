6e577b2428923024449f971d1213cf38d9569caf
kaikySantos <info.kaiky@gmail.com>
2023-01-29 18:11:11 -0300

    feat: component - loading

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, View } from 'native-base';
 import { THEME } from './src/theme';
 
-import { Text } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
